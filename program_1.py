81e5166c19ab92403ec41322ae0fe4cadb3264f4
mhtrost <mhtrost@students.unwsp.edu>
2026-01-28 12:58:53 -0600

    Assignment 2 Bob

--- program_1.py
+++ program_1.py
@@ -1,5 +1,14 @@
+#A fictional character's name
+#Fictional address with city, state and ZIP
+#Fictional telephone number
+#A college major
 def personal_information():
-    print('...')
+    print('Bob Stuart')
+    print('39176 Peach Rd. S')
+    print('Stillwater, MN')
+    print('         55067')
+    print('(763)540-3961')
+    print('Mr. Stuart has a B.S. degree in Fine Arts')
 
 
 # Line which calls the above function.
